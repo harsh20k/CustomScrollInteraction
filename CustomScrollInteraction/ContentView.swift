e97b46eee6d43c9ffeec1c3dd507c2d4ca731b83
harsh <harsh.pandey77@gmail.com>
2025-03-01 15:01:46 +0530

    Initial Commit

---
+++ CustomScrollInteraction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomScrollInteraction
+//
+//  Created by harsh  on 01/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
