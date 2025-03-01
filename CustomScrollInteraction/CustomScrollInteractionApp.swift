a61951de7145644ab223d8a8c366b20edc2cc74d
harsh <bighugscafe@gmail.com>
2025-03-01 15:01:46 +0530

    Initial Commit

---
+++ CustomScrollInteraction/CustomScrollInteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomScrollInteractionApp.swift
+//  CustomScrollInteraction
+//
+//  Created by harsh  on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomScrollInteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
